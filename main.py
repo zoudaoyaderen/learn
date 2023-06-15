9863c1e067afcf55a19273f124ac860bb7d31216
guihang <492794180@qq.com>
2023-06-15 14:39:23 +0800

    u

--- main.py
+++ main.py
@@ -639,6 +639,29 @@ def reorderList(head: ListNode) -> None:
     vec[i].next = None
 
 
+def dfs(grid, r, c):
+    grid[r][c] = "0"
+    nr, nc = len(grid), len(grid[0])
+    for x, y in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+        if 0 <= x < nr and 0 <= y < nc and grid[x][y] == "1":
+            dfs(grid, x, y)
+
+
+def numIslands(grid):
+    nr = len(grid)
+    if nr == 0:
+        return 0
+    nc = len(grid[0])
+
+    num_islands = 0
+    for r in range(nr):
+        for c in range(nc):
+            if grid[r][c] == "1":
+                num_islands += 1
+                dfs(grid, r, c)
+    return num_islands
+
+
 """
 # self_attention
 q.shape = B,nh,T,hs
